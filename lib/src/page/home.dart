a3ea3ed825371a5e7c6e07db0a48d03accf7f81b
arjun <arjun.abdillah30@gmail.com>
2021-12-16 07:56:56 +0700

    remove unused currentTime variable

--- lib/src/page/home.dart
+++ lib/src/page/home.dart
@@ -19,7 +19,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late DateTime time;
-  final currentTime = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
